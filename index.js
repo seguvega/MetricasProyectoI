c204e0dd46eede720c819b3fe1efe65aeb31b65a
seguvega <seguvega@hotmail.com>
2020-07-29 01:59:23 -0500

    :v

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
-
+/// Para subir las variables de entorno a heroku no se utilizan comillas ni nada de eso :v 
 
 app.listen(app.get('port'), () => {
     console.log("Server on Port", app.get('port'));
